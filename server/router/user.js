fc86036d2cd719ac86fe9bd1aa44bd8256e52a38
LucasNatoli <lucas.natoli@gmail.com>
2024-06-11 18:00:15 -0300

    agregados tickers y delete de favs

--- server/router/user.js
+++ server/router/user.js
@@ -4,6 +4,9 @@ const { decodeToken } = require("./middleware");
 const config = require("../config");
 const { parseFile, writeJson } = require("./fs-tools");
 
+/**
+ *  APIS 
+ */
 function addApi(req, res) {
   const t = Date.now();
   const path = `${req.decoded.userFolder}/${config.API_FILE}.json`;
@@ -31,79 +34,6 @@ function getApis(req, res) {
     }
     res.status(200).send(filtered);
   } catch (error) {
-    res.status(500).send();
-  }
-}
-
-function addFav(req, res) {
-  const t = Date.now();
-  const { symbol } = req.body;
-  const path = `${req.decoded.userFolder}/${config.FAVS_FILE}.json`;
-  try {
-    var content = parseFile(path);
-    //idempotente - si ya existia no hago nada
-    const filter = content.filter((i) => i.symbol == symbol);
-    if (filter.length == 0) content.push({ t, symbol });
-    writeJson(path, content);
-    res.status(201).send();
-  } catch (error) {
-    console.log(error);
-    res.status(500).send();
-  }
-}
-
-function getFavs(req, res) {
-  const path = `${req.decoded.userFolder}/${config.FAVS_FILE}.json`;
-  try {
-    var content = parseFile(path);
-    res.status(200).send(content);
-  } catch (error) {
-    res.status(500).send();
-  }
-}
-
-function updateFav (req, res){
-  //hacer lo mismo que updateApi pero con el archivo favs.json
-  const { t, symbol } = req.body;
-  var archivoNuevo = []
-  //armar el path del archivo api del usuario
-  const path = `${req.decoded.userFolder}/${config.FAVS_FILE}.json`;
-  try {
-    //parseFile del archivo del usuario
-    const content = parseFile(path);
-
-    //recorriendo el archivo que ahora es un array...
-    for (let x = 0; x < content.length; x++) {
-      //buscar dentro del array la t que corresponda
-      const element = content[x];
-      if (element.t === t) {
-        //actualizar los valores n, k, s
-        element.symbol = symbol;
-      } 
-      //agrego al archivo de salido nuevo
-      archivoNuevo.push(element)
-      
-    }
-    //guardar el archivo con los nuevos dtos
-    writeJson(path, archivoNuevo)
-    //devolver ok 200
-    res.status(200).send()
-  } catch (error) {
-    //por el contrario devolver 500
-    res.status(500).send();
-  }
-}
-
-function getAssets(req, res) {
-  const path = `${req.decoded.userFolder}/${config.ASSETS_FOLDER}`;
-  if (!existsSync(path)) return [];
-  try {
-    const files = readdirSync(path);
-    const jsonFiles = files.filter((file) => extname(file) === ".json");
-    const content = jsonFiles.map((file) => parse(file).name);
-    res.status(200).send(content);
-  } catch (error) {
-    console.log(error);
     res.status(500).send();
   }
 }
@@ -169,6 +99,198 @@ function deleteApi(req, res) {
   }
 }
 
+
+/**
+ *  ASSETS 
+ */
+function getAssets(req, res) {
+  const path = `${req.decoded.userFolder}/${config.ASSETS_FOLDER}`;
+  if (!existsSync(path)) return [];
+  try {
+    const files = readdirSync(path);
+    const jsonFiles = files.filter((file) => extname(file) === ".json");
+    const content = jsonFiles.map((file) => parse(file).name);
+    res.status(200).send(content);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+}
+
+function addFav(req, res) {
+  const t = Date.now();
+  const { symbol } = req.body;
+  const path = `${req.decoded.userFolder}/${config.FAVS_FILE}.json`;
+  try {
+    var content = parseFile(path);
+    //idempotente - si ya existia no hago nada
+    const filter = content.filter((i) => i.symbol == symbol);
+    if (filter.length == 0) content.push({ t, symbol });
+    writeJson(path, content);
+    res.status(201).send();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+}
+
+function getFavs(req, res) {
+  const path = `${req.decoded.userFolder}/${config.FAVS_FILE}.json`;
+  try {
+    var content = parseFile(path);
+    res.status(200).send(content);
+  } catch (error) {
+    res.status(500).send();
+  }
+}
+
+function updateFav (req, res){
+  //hacer lo mismo que updateApi pero con el archivo favs.json
+  const { t, symbol } = req.body;
+  var archivoNuevo = []
+  //armar el path del archivo api del usuario
+  const path = `${req.decoded.userFolder}/${config.FAVS_FILE}.json`;
+  try {
+    //parseFile del archivo del usuario
+    const content = parseFile(path);
+
+    //recorriendo el archivo que ahora es un array...
+    for (let x = 0; x < content.length; x++) {
+      //buscar dentro del array la t que corresponda
+      const element = content[x];
+      if (element.t === t) {
+        //actualizar los valores n, k, s
+        element.symbol = symbol;
+      } 
+      //agrego al archivo de salido nuevo
+      archivoNuevo.push(element)
+      
+    }
+    //guardar el archivo con los nuevos dtos
+    writeJson(path, archivoNuevo)
+    //devolver ok 200
+    res.status(200).send()
+  } catch (error) {
+    //por el contrario devolver 500
+    res.status(500).send();
+  }
+}
+
+function deleteFav (req, res){
+  const { t } = req.body;
+  var archivoNuevo = []
+  //armar el path del archivo api del usuario
+  const path = `${req.decoded.userFolder}/${config.FAVS_FILE}.json`;
+  try {
+    //parseFile del archivo del usuario
+    const content = parseFile(path);
+
+    //recorriendo el archivo que ahora es un array...
+    for (let x = 0; x < content.length; x++) {
+      //buscar dentro del array la t que corresponda
+      const element = content[x];
+      if (element.t !== t) {        
+        //agrego al archivo de salido nuevo
+        archivoNuevo.push(element)
+      } 
+    }
+    //guardar el archivo con los nuevos dtos
+    writeJson(path, archivoNuevo)
+    //devolver ok 204
+    res.status(204).send()
+  } catch (error) {
+    //por el contrario devolver 500
+    res.status(500).send();
+  }
+}
+
+function addTicker (req, res){
+  const t = Date.now();
+  const { symbol } = req.body;
+  const path = `${req.decoded.userFolder}/${config.TICKERS_FILE}.json`;
+  try {
+    var content = parseFile(path);
+    //idempotente - si ya existia no hago nada
+    const filter = content.filter((i) => i.symbol == symbol);
+    if (filter.length == 0) content.push({ t, symbol });
+    writeJson(path, content);
+    res.status(201).send();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+}
+
+function getTickers (req,res){
+  const path = `${req.decoded.userFolder}/${config.TICKERS_FILE}.json`;
+  try {
+    var content = parseFile(path);
+    res.status(200).send(content);
+  } catch (error) {
+    res.status(500).send();
+  }
+}
+
+function updateTicker (req,res){
+//hacer lo mismo que updateApi pero con el archivo tickers.json
+const { t, symbol } = req.body;
+var archivoNuevo = []
+//armar el path del archivo api del usuario
+const path = `${req.decoded.userFolder}/${config.TICKERS_FILE}.json`;
+try {
+  //parseFile del archivo del usuario
+  const content = parseFile(path);
+
+  //recorriendo el archivo que ahora es un array...
+  for (let x = 0; x < content.length; x++) {
+    //buscar dentro del array la t que corresponda
+    const element = content[x];
+    if (element.t === t) {
+      //actualizar los valores n, k, s
+      element.symbol = symbol;
+    } 
+    //agrego al archivo de salido nuevo
+    archivoNuevo.push(element)
+    
+  }
+  //guardar el archivo con los nuevos dtos
+  writeJson(path, archivoNuevo)
+  //devolver ok 200
+  res.status(200).send()
+} catch (error) {
+  //por el contrario devolver 500
+  res.status(500).send();
+}
+}
+
+function deleteTicker (req, res){
+  const { t } = req.body;
+  var archivoNuevo = []
+  //armar el path del archivo api del usuario
+  const path = `${req.decoded.userFolder}/${config.TICKERS_FILE}.json`;
+  try {
+    //parseFile del archivo del usuario
+    const content = parseFile(path);
+
+    //recorriendo el archivo que ahora es un array...
+    for (let x = 0; x < content.length; x++) {
+      //buscar dentro del array la t que corresponda
+      const element = content[x];
+      if (element.t !== t) {        
+        //agrego al archivo de salido nuevo
+        archivoNuevo.push(element)
+      } 
+    }
+    //guardar el archivo con los nuevos dtos
+    writeJson(path, archivoNuevo)
+    //devolver ok 204
+    res.status(204).send()
+  } catch (error) {
+    //por el contrario devolver 500
+    res.status(500).send();
+  }
+}
+
 module.exports = (app) => {
   const baseUrl = "/user";
   const apiUrl = baseUrl + "/apis";
@@ -182,5 +304,10 @@ module.exports = (app) => {
   app.post(favsUrl, decodeToken, addFav);
   app.put(favsUrl, decodeToken, updateFav);
   app.get(favsUrl, decodeToken, getFavs);
+  app.delete(favsUrl, decodeToken, deleteFav);
   app.get(assetsUrl, decodeToken, getAssets);
+  app.post(tickersUrl, decodeToken, addTicker);
+  app.get(tickersUrl, decodeToken, getTickers);
+  app.put(tickersUrl, decodeToken, updateTicker);
+  app.delete(tickersUrl, decodeToken, deleteTicker);
 };
